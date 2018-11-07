6754421ee2fc0a8dc472c85e960931ac6ea0c706
Henry Fung <henryfung3a27@yahoo.com.hk>
2018-11-07 02:23:51 +0000

    Added comments

--- exercise/rec_fac.s
+++ exercise/rec_fac.s
@@ -16,24 +16,29 @@ syscall
 
 
 FAC:
-addi $sp, $sp, -4   # n
-sw $ra, 0($sp)      # | store the state
-#sw $s0, 4($sp)      # u
+# store the state
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+#sw $s0, 4($sp)
 
+# check $a0 <= 1
 addi $s0, $a0, 0        # $s0 = $a0(x)
 subi $t0, $s0, 1        # $t0 = $s0 - 1 (to compare)
 blez $t0, EXIT_FAC      # ($t0 <= 1) -> return_ans
+
+# calculate
 mul $v0, $v0, $s0       # res = res * $s0
 subi $a0, $a0, 1        # $a0(x) -= 1
 jal FAC
 
 EXIT_FAC:
+# Restore the state of $ra
 lw $ra, 0($sp)
 #lw $s0, 4($sp)
 addi $sp, $sp, 4
 jr $ra
 
-# In line 19 and 33, the 4 can be canged to 8 and
-# uncomment line 21 and 32.
+# In line 20 and 38, the 4 can be changed to 8 and
+# uncomment line 22 and 37.
 # Not sure the meaning of storing the state
 # of $s0 yet
