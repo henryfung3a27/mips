c7c49d3589291b4cd5d6f4a6947e044779a53956
Henry Fung <henryfung3a27@yahoo.com.hk>
2018-11-22 22:22:22 +0000

    Added comments

--- exercise/fibonancci/fib.s
+++ exercise/fibonancci/fib.s
@@ -58,27 +58,30 @@ GENERATE_FIB:
     addi $t0, $a2, -1       # $t0 = current - 1
     beqz $t0, INSERT_ONE    # if $t0 = 0 ($a2 = 1), insert one to heap
     addi $t1, $a2, -2       # $t1 = current - 2
+# Get the value of fib[current-1] and fib[current-2] from heap    
     mul $t0, $t0, $t9       # $t0 *= 4 bytes of int
     mul $t1, $t1, $t9       # $t1 *= 4 bytes of int
     add $t0, $s1, $t0       # $t0 = address of fib[current-1]
     add $t1, $s1, $t1       # $t1 = address of fib[current-2]
     lw $t0, ($t0)           # $t0 = fib[current-1]
     lw $t1, ($t1)           # $t1 = fib[current-2]
+# Store the sum of fib[current-1] and fib[current-2] to heap
     add $t0, $t0, $t1       # $t0 = fib[current-1] + fib[current-2]
     sw $t0, ($a1)           # store $t0 to result heap
+# Increment address of heap and current
     add $a1, $a1, $t9       # increment address of result heap (+4)
-    addi $a2, $a2, 1        # increment current m'th fib number
+    addi $a2, $a2, 1        # increment current n'th fib number
     j GENERATE_FIB
 # Insert 0 to fib sequence heap
 INSERT_ZERO:
-    li $t0, 0
+    li $t0, 0				# $t0 = 0
     sw $t0, ($a1)           # ($a1) = 0
     add $a1, $a1, $t9       # $a1 += 4 (pointer to result heap + 4)
     addi $a2, $a2, 1        # $a2 += 1 (current += 1)
     j GENERATE_FIB
 # Insert 1 to fib sequence heap
 INSERT_ONE:
-    li $t0, 1
+    li $t0, 1				# $t0 = 1
     sw $t0, ($a1)           # ($a1) = 1
     add $a1, $a1, $t9       # $a1 += 4 (pointer to result heap + 4)
     addi $a2, $a2, 1        # $a2 += 1 (current += 1)
