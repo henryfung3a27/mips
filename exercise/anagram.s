95de66ba350d21d90b956072105e2c6c15b1002d
Henry Fung <henryfung3a27@yahoo.com.hk>
2018-11-22 22:01:01 +0000

    Compiled and tested on MARS, still has error on Simulizer
    Error message:
      sbrk requested extends past maximum heap length..
      Details: {breakOfHeap = 144, heapSize=1048576}

--- exercise/anagram.s
+++ exercise/anagram.s
@@ -12,7 +12,7 @@ L:      .asciiz "abc"
         .asciiz "cde"
         .asciiz "dde"
         .asciiz "dec"
-	
+nl:     .asciiz "\n"
     .text
 ### ### ### ### ### ###
 ### MainCode Module ###
@@ -24,6 +24,12 @@ main:
     la $s1,s                # $s1: key word
     lw $s2,n                # $s2: size of string list
     
+# sort key word
+    li $a2, 0               # $a2: left of merge_sort (0)
+    subi $a3, $s0, 2        # $a3: right of metge_sort (k-2)
+    move $s7, $s1           # $s7: key word
+    jal MERGE_SORT
+    move $s1, $s7           # $s1 = sortted key word
 # allocate heap space for string array:    
     li $v0,9                # syscall code 9: allocate heap space
     mul $a0,$s2,$t9         # calculate the amount of heap space
@@ -42,35 +48,45 @@ READ_DATA:
     addi $t1,$t1,4
     add $t2,$t2,$s0
     j READ_DATA
- 
+
 FIND:
-### write your code ###
-    beq $t0, $s2, PRINT_RESULT  # jump PRINT_RESULT if $t0==$s2 (i == n)
-    addi $t0, $t0, 1            # increment $t0 (size)
-    addi $t1, $t1, -4           # decrement $t1 for loading word
+    beqz $s2, PRINT_RESULT      # jump PRINT_RESULT if $t0==$s2 (i == n)
     li $a2, 0                   # $a2: left of merge_sort  (0)
     subi $a3, $s0, 2            # $a3: right of merge_sort (k-2)
-    lw $t5, ($t1)               # $t5: the string to be sorted
-    jal MERGE_SORT              # return string -> $a0
+    lw $s7, ($s3)               # $s7: the string to be sorted
+    addi $s2, $s2, -1           # decrement $s2 (size of list L)
+    addi $s3, $s3, 4            # increment $s3 for loading word
+    
+    jal MERGE_SORT              # return string -> $s7
+# compare the sortted string and sortted key word
+    move $a0, $s7               # $a0: sortted word from list
+    move $a1, $s1               # $a1: sortted key word
+    subi $a2, $s0, 1            # $a2: length of strings (k-1)
     jal COMPARE                 # return value -> $a0
-    bne $a0, $zero, FIND        # if $a0 != 0, recurse
-    addi $s4, $s4, 1            # result counter += 1
+    add $s4, $s4, $a0           # result counter += $a0 (1 if equal, 0 otherwise)
     j FIND
 
-### @param $t5 input string in heap
+### print the result counter
+PRINT_RESULT:
+    move $a0, $s4           # $s4: result counter
+    li $v0, 1               # print int
+    syscall
+    j EXIT                  # exit the program
+
+### @param $s7 input string in heap
 #          $a2 left
 #          $a3 right
 MERGE_SORT:
-    sub $t0, $a2, $a3           # $t0 = left - right
-    bgez $t0, FINISH_RA         # if left >= right return
-    addi $sp, $sp, -4
+    sub $t0, $a3, $a2           # $t0 = right - left
+    addi $sp, $sp, -4           # store the state of $ra
     sw $ra, ($sp)
+    beqz $t0, FINISH_RA         # if left >= right return
 # split the string into 2
     li $t1, 2                   # $t1 = constant 2
     add $t0, $a2, $a3           # $t0 = left + right
     div $t0, $t0, $t1           # $t0 = (left + right) / 2
     mflo $t0                    # $t0 = quotient of result (mid)
-    addi $sp, $sp, -12
+    addi $sp, $sp, -12          # allocate 3 word spaces
     sw $t0, ($sp)               # 0($sp) = mid
     sw $a2, 4($sp)              # 4($sp) = left
     sw $a3, 8($sp)              # 8($sp) = right
@@ -79,11 +95,79 @@ MERGE_SORT:
     move $a3, $t0               # $a3 = mid
     jal MERGE_SORT
 # MERGE_SORT RHS of array
-    addi $a2, $t0, 1            # $a2 = mid+1
-    lw $a3, 8($sp)              # $a3 = right
+    lw $a2, 0($sp)              # load mid from stack
+    addi $a2, $a2, 1            # $a2 = mid+1
+    lw $a3, 8($sp)              # $a3 = right (load from stack)
     jal MERGE_SORT
 # MERGING
+    move $a0, $s0               # create result heap with size k
+    li $v0, 9
+    syscall
+    move $s6, $v0               # $s6 = base of result heap
+# create heap for left_array
+    move $a0, $s0               # size of sub-array (k)
+    li $v0, 9
+    syscall
+    move $t4, $v0               # always point to base of left-heap
+    move $t5, $v0               # for CREATE_LEFT to increment
+# create heap for right_array
+    li $v0, 9
+    syscall
+    move $t6, $v0               # always point to base of right-heap
+    move $t7, $v0               # for CREATE_RIGHT to incrementlw $a0, 4($sp)
+    
+    lw $a0, 4($sp)              # $a0 = left
+    lw $a1, 0($sp)
+    addi $a1, $a1, 1            # $a1 = mid+1
+    lw $a2, 8($sp)              # $a2 = right
+# insert char into left heap from left to mid
+CREATE_LEFT:
+    sub $t0, $a0, $a1           # $t0 = $a0 - $a1 = left - (mid+1)
+    beqz $t0, CREATE_RIGHT
+    add $t0, $s7, $a0           # $t0 = str + left
+    lb $t0, ($t0)               # $t0 = str[left]
+    sb $t0, ($t5)               # store $t0 to $t5
+    addi $t5, $t5, 1            # increment $t5 (heap)
+    addi $a0, $a0, 1            # increment $a0 (left)
+    j CREATE_LEFT
+# insert char into right heap from mid+1 to right
+CREATE_RIGHT:
+    sub $t0, $a1, $a2           # $t0 = $a1 - $a2 = (mid+1) - right
+    subi $t0, $t0, 1            # $t0 = $t0 - 1
+    beqz $t0, DONE_CREATE
+    add $t0, $s7, $a1           # $t0 = str + (mid+1)
+    lb $t0, ($t0)               # $t0 = str[mid+1]
+    sb $t0, ($t7)               # store $t0 to $t7
+    addi $t7, $t7, 1            # increment $t7 (heap)
+    addi $a1, $a1, 1            # increment $a1 (mid+1)
+    j CREATE_RIGHT
+# call MERGING with
+# $a0 = base of heap of left-array (null-terminated)
+# $a1 = base of heap of right-array (null-terminated)
+# $a2 = base of result heap (null-terminated on return by MERGING)
+DONE_CREATE:
+    move $a0, $t4               # $a0 = base of left heap
+    move $a1, $t6               # $a1 = base of right heap
+    move $a2, $s6               # $a2 = base of result heap
+    jal MERGING
 
+# insert the result heap into input heap
+    move $a0, $s6               # $a0 = base of result heap
+    lw $t0, 4($sp)              # $t0 = left of string
+    lw $t1, 8($sp)              # $t1 = right of string
+    move $t2, $s7               # $t2 = base of input string
+    add $t2, $t2, $t0           # $t2 = pointer to left of input string
+                                #     = string[left]
+INSERT_INTO_HEAP:
+    lb $t3, ($a0)               # $t3 = byte from result heap
+    beqz $t3, FINISH_INSERT     # if $t3=0 then finish
+    sb $t3, ($t2)               # store the byte to ($t2) string[left]
+    addi $a0, $a0, 1            # increment result heap address
+    addi $t2, $t2, 1            # increment input string address
+    j INSERT_INTO_HEAP
+FINISH_INSERT:
+    addi $sp, $sp, 12           # free stack with left, mid and right
+    j FINISH_RA
 
     
 # @param $a0 base of string_1 heap
@@ -97,9 +181,6 @@ MERGING:
     lb $t1, ($a1)           # $t1 = str2[0]
     beqz $t0, MERGE_STR2    # chech if one of the first char
     beqz $t1, MERGE_STR1    # is \0, branch if yes
-# store $ra
-    addi $sp, $sp, -4
-    sw $ra, ($sp)
 # compare
     ble $t0, $t1, CHOOSE_1  # jump CHOOSE_1 if $t0 <= $t1
 # CHOOSE_2:
@@ -117,8 +198,6 @@ CHOOSE_1:
 MERGE_STR1:
     lb $t0, ($a0)               # $t0 = load byte from $a0 (str1)
     beqz $t0, FINISH_MERGING    # return if $t0 = 0 (\0)
-    addi $sp, $sp, -4           # store $ra
-    sw $ra, ($sp)
     sb $t0, ($a2)               # store $t0 to $a2 (result heap)
     addi $a2, $a2, 1            # increment result heap address
     addi $a0, $a0, 1            # increment str1 address
@@ -128,8 +207,6 @@ MERGE_STR1:
 MERGE_STR2:
     lb $t0, ($a1)               # $t0 = load byte from $a1 (str2)
     beqz $t0, FINISH_MERGING    # return if $t0 = 0 (\0)
-    addi $sp, $sp, -4           # store $ra
-    sw $ra, ($sp)
     sb $t0, ($a2)               # store $t0 to $a2 (result heap)
     addi $a2, $a2, 1            # increment result heap address
     addi $a1, $a1, 1            # increment str2 address
@@ -138,7 +215,7 @@ MERGE_STR2:
 ### put null char to the end of result heap
 FINISH_MERGING:
     sb $zero, ($a2)
-    j FINISH_RA
+    jr $ra
 
 ### compare two strings (key string and string)
 # @param  $a0 base of heap_1
@@ -147,9 +224,6 @@ FINISH_MERGING:
 # @return $a0 0 if equal, 1 otherwise
 COMPARE:
     beqz $a2, EQUAL             # return EQUAL if size=0
-# store $ra
-    addi $sp, $sp, -4
-    sw $ra, ($sp)
 # size -= 1
     addi $a2, $a2, -1
 # a0[0] - a1[0]
@@ -162,11 +236,11 @@ COMPARE:
 # recurse
     j COMPARE
 EQUAL:
-    li $a0, 0                   # $a0 = 0
-    j FINISH_RA
+    li $a0, 1                   # $a0 = 1
+    jr $ra
 NOT_EQUAL:
-    li $a0, 1                   # $a1 = 1
-    j FINISH_RA
+    li $a0, 0                   # $a1 = 0
+    jr $ra
 
 # this sub-routine helps recover the state of $ra
 # stored in stack and jr it
@@ -175,52 +249,26 @@ FINISH_RA:
     addi $sp, $sp, 4
     jr $ra
 
-### print the result counter
-PRINT_RESULT:
-    move $a0, $s4           # $s4: result counter
-    li $v0, 1               # print int
-    syscall
-    j EXIT                  # exit the program
-    
-# @param $a0 base address of input heap
-#        $a1 base address of result heap
-#        $a2 size
-# the last element in the heap will be 0 and points to it
-# i.e. $a1 = [1, 2, 3, 0] for size 4
-#                      ^ $a1 points here at the end
-TO_HEAP:
-    beqz $a2, FINISH_TO_HEAP    # return if size = 0
-    addi $sp, $sp, -4           # store the state of $ra
-    sw $ra, ($sp)
-    lb $t0, ($a0)               # $t0: load byte from string
-    sb $t0, ($a1)               # store the byte from $t0 to $a1
-    addi $a0, $a0, 1            # increment $a0 (string pointer)
-    addi $a1, $a1, 1            # increment $a1 (heap pointer)
-    addi $a2, $a2, -1           # decrement $a2 (size)
-    j TO_HEAP
-    
-# put 0 to the end of the heap before returning
-FINISH_TO_HEAP:
-    sb $zero, ($a1)
-    lw $ra, ($sp)
-    addi $sp, $sp, 4
-    jr $ra
-
 # @param $a0 base of heap address
 #        $a1 size
 PRINT_HEAP:
-    beqz $a1, FINISH_RA     # if size=0 return
+    beqz $a1, FINISH_PRINT_HEAP    # if size=0 return
     addi $sp, $sp, -8
     sw $a0, 0($sp)          # store the state of $a0
     sw $ra, 4($sp)          # store the state of $ra
     lb $a0, ($a0)           # n
-    li $v0, 11              # |> print byte
+    li $v0, 1              # |> print byte
     syscall                 # u
     lw $a0, ($sp)           # restore the state of $a0
     addi $sp, $sp, 4
     addi $a0, $a0, 1        # increment pointer of heap
     addi $a1, $a1, -1       # decrement the size
     j PRINT_HEAP
+FINISH_PRINT_HEAP:
+    la $a0, nl
+    li $v0, 4
+    syscall
+    j FINISH_RA
     
 EXIT:
     li $v0, 10
